168537849cdc087044b0bf5051ffff8f13928b86
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2022-12-23 19:30:03 +0800

    feat(Config, APIs): Added debug.enable_function and exported HeliumExtensionLogger

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,17 +1,40 @@
 ﻿#include "pch.h"
 #include "Helium.h"
 
-int on_self_load()
+#include <map>
+#include <string>
+
+using namespace std;
+
+Helium::HeliumExtensionLogger logger("HeliumBackup", "Main");
+
+HELIUM_EXTENSION_EXPORT map<string, string> extension_metadata()
+{
+	return {
+		{"name", "Helium Backup"},
+		{"author", "Helium DevTeam"},
+		{"url", "https://github.com/Helium-DevTeam/HeliumBackup"},
+		{"description", "A official backup extension for Helium."},
+		{"helium_version", ">=0.8.38-alpha.0"}
+	};
+}
+
+HELIUM_EXTENSION_EXPORT int on_self_load()
 {
 	auto root = Helium::command_dispatcher.Register("#hback");
 	auto create = root.Then<Literal>("create");
 	auto restore = root.Then<Literal>("restore");
 	auto list = root.Then<Literal>("list");
 	auto del = root.Then<Literal>("delete");
+	logger.info("awa");
+	return 0;
 }
 
-int on_load()
+HELIUM_EXTENSION_EXPORT int on_load()
 {
 	const auto server_ptr = Helium::helium_server_manager.GetServer("test1");
 	auto dir = server_ptr->GetServerDirectory();
+	const auto extension_ptr = Helium::helium_extension_manager.GetExtension("HeliumBackup");
+	auto d = extension_ptr->GetExtensionPath();
+	return 0;
 }
