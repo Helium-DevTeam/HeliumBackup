1f0437635b52b2212dc52e87452aa795f18049d9
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2022-12-26 20:19:04 +0800

    feat: A try

--- dllmain.cpp
+++ dllmain.cpp
@@ -7,7 +7,7 @@
 #include <chrono>
 
 using namespace std;
-using namespace Helium;
+using namespace helium;
 namespace fs = std::filesystem;
 
 HeliumExtensionLogger logger("HeliumBackup", "Main");
@@ -23,24 +23,24 @@ HELIUM_EXTENSION_EXPORT map<string, string> extension_metadata()
 	};
 }
 
-int hback_list(HeliumCommandContext& ctx)
+int hback_list(helium_command_context& ctx)
 {
 	logger.debug("#hback list!");
 	return 0;
 }
 
-int hback_create(HeliumCommandContext& ctx)
+int hback_create(helium_command_context& ctx)
 {
-	if (ctx.GetSource().GetSource() != CommandSource::PLAYER)
+	if (ctx.GetSource().get_source() != command_source_e::PLAYER)
 	{
 		logger.error("This command only work in servers");
 		return -1;
 	}
 	using namespace std::literals::chrono_literals;
-	const auto server_name = ctx.GetSource().GetSourceInfo()["server_name"];
+	const auto server_name = ctx.GetSource().get_source_info()["server_name"];
 	const auto server_ptr = helium_server_manager.GetServer(server_name);
-	fs::path server_save_path = helium_config_manager.GetServerDir() + "/" + server_ptr->GetServerDirectory().string() + "/world";
-	fs::path copy_save_path = helium_config_manager.GetExtensionDir() + "/Helium-Backup/" + format("{:%F}-{:%H}-{:%M}-{:%S}"
+	fs::path server_save_path = helium_config_manager.get_server_dir() + "/" + server_ptr->GetServerDirectory().string() + "/world";
+	fs::path copy_save_path = helium_config_manager.get_extension_dir() + "/Helium-Backup/" + format("{:%F}-{:%H}-{:%M}-{:%S}"
 		, chrono::system_clock::now(), chrono::system_clock::now(), chrono::system_clock::now(), chrono::system_clock::now());
 	logger.debug("Saving server " + server_name);
 	logger.debug(server_save_path.string());
@@ -61,13 +61,13 @@ int hback_create(HeliumCommandContext& ctx)
 	return 0;
 }
 
-int hback_restore(HeliumCommandContext& ctx)
+int hback_restore(helium_command_context& ctx)
 {
 	logger.debug("#hback restore!");
 	return 0;
 }
 
-int hback_delete(HeliumCommandContext& ctx)
+int hback_delete(helium_command_context& ctx)
 {
 	logger.debug("#hback delete!");
 	return 0;
@@ -75,7 +75,7 @@ int hback_delete(HeliumCommandContext& ctx)
 
 HELIUM_EXTENSION_EXPORT int on_self_load()
 {
-	using enum HeliumCapabilities;
+	using enum helium_capabilities_e;
 
 	logger.debug("Hello Helium Extension World!");
 	logger.debug(fs::current_path().string());
@@ -86,27 +86,27 @@ HELIUM_EXTENSION_EXPORT int on_self_load()
 	auto list = root.Then<Literal>("list");
 	auto del = root.Then<Literal>("delete");
 
-	create.Then<Argument, String>("<backup-name>").Then<Argument, GreedyString>("[comment]").Requires([](HeliumCommandSource& src) -> bool
+	create.Then<Argument, String>("<backup-name>").Then<Argument, GreedyString>("[comment]").Requires([](helium_command_source_c& src) -> bool
 		{
-			return HeliumUserManager::CheckCapability(src.GetCapabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+			return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
 		}).Executes(hback_create);
-		create.Then<Argument, String>("<backup-name>").Requires([](HeliumCommandSource& src) -> bool
+		create.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
 			{
-				return HeliumUserManager::CheckCapability(src.GetCapabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+				return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
 			}).Executes(hback_create);
-			restore.Then<Argument, String>("<backup-name>").Requires([](HeliumCommandSource& src) -> bool
+			restore.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
 				{
-					return HeliumUserManager::CheckCapability(src.GetCapabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+					return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
 				}).Executes(hback_restore);
-				list.Requires([](HeliumCommandSource& src) -> bool
+				list.Requires([](helium_command_source_c& src) -> bool
 					{
-						if (src.GetSource() == CommandSource::PLAYER)
-						logger.debug(src.GetSourceInfo().at("player_name"));
-				return HeliumUserManager::CheckCapability(src.GetCapabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+						if (src.get_source() == command_source_e::PLAYER)
+						logger.debug(src.get_source_info().at("player_name"));
+				return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
 					}).Executes(hback_list);
-					del.Then<Argument, String>("<backup-name>").Requires([](HeliumCommandSource& src) -> bool
+					del.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
 						{
-							return HeliumUserManager::CheckCapability(src.GetCapabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+							return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
 						}).Executes(hback_delete);
 
 						return 0;
