fdb4d29e0bf221922e2c318f3637b3a25457679e
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2022-12-27 10:57:33 +0800

    docs(readme): Update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+![](E:\Helium\Helium-Official-Extension%20(1080P).png)
+
 # HeliumBackup
+
+An official backup extension for Helium
--- dllmain.cpp
+++ dllmain.cpp
@@ -6,6 +6,8 @@
 #include <format>
 #include <chrono>
 
+#include <boost/locale.hpp>
+
 using namespace std;
 using namespace helium;
 namespace fs = std::filesystem;
@@ -45,19 +47,28 @@ int hback_create(helium_command_context& ctx)
 	logger.debug("Saving server " + server_name);
 	logger.debug(server_save_path.string());
 	logger.debug(copy_save_path.string());
+
 	error_code ec;
 	try {
+		logger.debug("save-off");
 		server_ptr->SendToServer("/save-off");
-		this_thread::sleep_for(200ms);
+		this_thread::sleep_for(15s);
+		logger.debug("save-all flush");
 		server_ptr->SendToServer("/save-all flush");
-		fs::copy(server_save_path, copy_save_path, fs::copy_options::recursive | fs::copy_options::overwrite_existing, ec);
+		//this_thread::sleep_for(15s);
+		//logger.debug("copy");
+		//fs::copy(server_save_path, copy_save_path, fs::copy_options::recursive | fs::copy_options::overwrite_existing);
+		this_thread::sleep_for(15s);
+		logger.debug("save-on");
 		server_ptr->SendToServer("/save-on");
 	}
-	catch (exception& e)
+	catch (fs::filesystem_error& e)
 	{
+		logger.debug("save-on");
+		server_ptr->SendToServer("/save-on");
 		logger.error(e.what());
-		logger.error(ec.message());
 	}
+
 	return 0;
 }
 
@@ -90,24 +101,24 @@ HELIUM_EXTENSION_EXPORT int on_self_load()
 		{
 			return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
 		}).Executes(hback_create);
-		create.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
-			{
-				return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
-			}).Executes(hback_create);
-			restore.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
-				{
-					return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
-				}).Executes(hback_restore);
-				list.Requires([](helium_command_source_c& src) -> bool
-					{
-						if (src.get_source() == command_source_e::PLAYER)
-						logger.debug(src.get_source_info().at("player_name"));
-				return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
-					}).Executes(hback_list);
-					del.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
-						{
-							return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
-						}).Executes(hback_delete);
+	create.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
+		{
+			return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+		}).Executes(hback_create);
+	restore.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
+		{
+			return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+		}).Executes(hback_restore);
+	list.Requires([](helium_command_source_c& src) -> bool
+		{
+			if (src.get_source() == command_source_e::PLAYER)
+				logger.debug(src.get_source_info().at("player_name"));
+			return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+		}).Executes(hback_list);
+	del.Then<Argument, String>("<backup-name>").Requires([](helium_command_source_c& src) -> bool
+		{
+			return helium_user_manager_c::check_capability(src.get_capabilities(), HCAP_BASIC_CTL | HCAP_SERVER_CTL);
+		}).Executes(hback_delete);
 
-						return 0;
+	return 0;
 }
